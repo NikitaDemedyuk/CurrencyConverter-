4ae57151ae15f53e9950e960be88da20bd094ca9
Nikita <nikitademedyuk1@gmail.com>
2022-02-09 21:25:11 +0300

    Step 3 (build view controllers)

--- CurrencyConverter/Controller/VC1.swift
+++ CurrencyConverter/Controller/VC1.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class VC1: UIViewController {
 
+    @IBOutlet weak var headLbl: UILabel!
+   
+    @IBOutlet weak var flagImg1: UIImageView!
+    
+    @IBOutlet weak var typeCur1: UILabel!
+    
+    @IBOutlet weak var txtField1: UITextField!
+    
+    @IBOutlet weak var flagImg2: UIImageView!
+    
+    @IBOutlet weak var typeCur2: UILabel!
+    
+    
+    @IBOutlet weak var txtField2: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- CurrencyConverter/Controller/VC2.swift
+++ CurrencyConverter/Controller/VC2.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class VC2: UIViewController {
 
+    @IBAction func goBack(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
