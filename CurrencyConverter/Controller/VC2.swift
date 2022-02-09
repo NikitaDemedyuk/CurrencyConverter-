ac9ffa26950761a76b83eaf5ddcca831b7675212
Nikita <nikitademedyuk1@gmail.com>
2022-02-09 20:04:24 +0300

    Step 1 ( launch screen)

---
+++ CurrencyConverter/Controller/VC2.swift
@@ -0,0 +1,15 @@
+//
+//  VC2.swift
+//  CurrencyConverter
+//
+//  Created by Никита on 9.02.22.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
