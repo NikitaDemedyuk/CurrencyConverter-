0f3f7895bb809eceb5c16e147f3413ad1c5fe409
Nikita <nikitademedyuk1@gmail.com>
2022-02-12 16:04:04 +0300

    Step 13

--- CurrencyConverter/Controller/VC1.swift
+++ CurrencyConverter/Controller/VC1.swift
@@ -72,31 +72,19 @@ class VC1: UIViewController, UITextFieldDelegate {
     
     }
     
-    @objc func editingBegan(_ textField: UITextField) {}
     
-    
-    @objc func editingChanged(_ textField: UITextField) {
-        //текст изменился
-    }
-
-    @objc func editingEnded(_ textField: UITextField) {
-      
-    }
-    
+   /*
     func checkString(string: String) -> Bool {
         let set = CharacterSet(charactersIn: string)
-        //if CharacterSet.decimalDigits.isSuperset(of: set) {
+        
+        if CharacterSet.decimalDigits.isSuperset(of: set) {
             return true
-        //} else {
-          //  return false
-        //}
+        } else {
+            return false
+        }
     }
+    */
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let allowedCharacters = CharacterSet.decimalDigits
-        let characterSet = CharacterSet(charactersIn: string)
-        return allowedCharacters.isSuperset(of: characterSet)
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first as? UITouch {
@@ -163,25 +151,13 @@ class VC1: UIViewController, UITextFieldDelegate {
     func calculate() {
         if !txtField1.text!.isEmpty || !txtField2.text!.isEmpty {
             if money1.boolFlag! {
-                if checkString(string: txtField1.text!) {
-                    let numField1 = Double(txtField1.text!)
-                    txtField2.text = String(numField1! / money1.currencyExchange * money2.currencyExchange)
-                } else {
-                    let alert = UIAlertController(title: "Error", message: "Text field must contain  only numbers!", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                }
+                let numField1 = Double(txtField1.text!)
+                txtField2.text = String(numField1! / money1.currencyExchange * money2.currencyExchange)
             }
 
             if money2.boolFlag! {
-                if checkString(string: txtField2.text!) {
-                    let numField2 = Double(txtField2.text!)
-                    txtField1.text = String(numField2! / money2.currencyExchange * money1.currencyExchange)
-                } else {
-                    let alert = UIAlertController(title: "Error", message: "Text field must contain  only numbers!", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                }
+                let numField2 = Double(txtField2.text!)
+                txtField1.text = String(numField2! / money2.currencyExchange * money1.currencyExchange)
             }
         } else {
             let alert = UIAlertController(title: "Empty fields", message: "Please, enter numbers at least in one field!", preferredStyle: .alert)
@@ -219,94 +195,3 @@ class VC1: UIViewController, UITextFieldDelegate {
 
 
 
-
-/*
-
-if typeCur1.text == "EUR"
- switch typeCur2.text {
-   case "USD":
-       let numField1 = Double(txtField1.text!)
-       txtField2.text = String(numField1! * 2.0)
-   case "RUS":
-       let numField1 = Double(txtField1.text!)
-       txtField2.text = String(numField1! * 6.0)
-   case "BYN":
-       let numField1 = Double(txtField1.text!)
-       txtField2.text = String(numField1! * 8.0)
-   default:
-       txtField2.text = txtField1.text
-   }
-}
-
-if typeCur2.text == "EUR" {
-   switch typeCur1.text {
-   case "USD":
-       let numField1 = Double(txtField2.text!)
-       var numField2: Double? = (numField1! * 2.0)
-       txtField1.text = String(numField2!)
-   case "RUS":
-       let numField1 = Double(txtField2.text!)
-       txtField1.text = String(numField1! * 6.0)
-   case "BYN":
-       let numField1 = Double(txtField2.text!)
-       txtField1.text = String(numField1! * 8.0)
-   default:
-       print(txtField1)
-   }
-}
-
-if typeCur1.text == "USD" {
-   switch typeCur2.text {
-   case "EUR":
-       let numField1 = Double(txtField1.text!)
-       var numField2: Double? = (numField1! * 0.5)
-       txtField2.text = String(numField2!)
-   case "RUS":
-       let numField1 = Double(txtField1.text!)
-       txtField2.text = String(numField1! * 3.0)
-   case "BYN":
-       let numField1 = Double(txtField1.text!)
-       txtField2.text = String(numField1! * 4.0)
-   default:
-       txtField2.text = txtField1.text
-   }
-}
-
-if typeCur2.text == "USD" {
-   switch typeCur1.text {
-   case "EUR":
-       let numField1 = Double(txtField2.text!)
-       var numField2: Double? = (numField1! * 0.5)
-       txtField1.text = String(numField2!)
-   case "RUS":
-       let numField1 = Double(txtField2.text!)
-       txtField1.text = String(numField1! * 3.0)
-   case "BYN":
-       let numField1 = Double(txtField2.text!)
-       txtField1.text = String(numField1! * 4.0)
-   default:
-       txtField1.text = txtField2.text
-   }
-}
- 
- 
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let numField1 = Double(txtField1.text!)
-// txtField2.text = String(numField1! * money2.currencyExchange2 * money1.currencyExchange)
